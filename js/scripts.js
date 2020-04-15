c1be5f5ce1b1dc3f3d40a065fd9427b01e2d5e90
PoliBarros <poli.barros@yahoo.com.br>
2020-04-14 23:37:56 -0400

    final

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,17 @@ function goComedy() {
 function goAnimation() {
     location.href = './animation.html';
 };
+
+$(document).ready(function() {
+    $("h3").fadeOut("slow");
+    $("h3").fadeIn("slow");
+
+    $("#advenDescr").text("Adventure film is a genre that revolves around the conquests and explorations " +
+        "of a protagonist. The purpose of the conquest can be to retrieve a person or treasure, but " +
+        "often the main focus is simply the pursuit of the unknown. These films " +
+        "generally take place in exotic locations and play on historical myths.");
+
+    $("#contact").text("Contact")
+
+
+});
